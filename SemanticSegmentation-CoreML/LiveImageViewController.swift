3fadd4f2e8a9e8c11a879f8b43139a16d5c4e685
Narasimhakommi <47457147+Narasimhakommi@users.noreply.github.com>
2023-04-24 15:12:27 -0400

    Update LiveImageViewController.swift

--- SemanticSegmentation-CoreML/LiveImageViewController.swift
+++ SemanticSegmentation-CoreML/LiveImageViewController.swift
@@ -99,13 +99,13 @@ class LiveImageViewController: UIViewController {
         videoCapture.setUp(sessionPreset: .vga640x480) { success in
             
             if success {
-                // UI에 비디오 미리보기 뷰 넣기
+                
                 if let previewLayer = self.videoCapture.makePreview() {
                     self.videoPreview.layer.addSublayer(previewLayer)
                     self.resizePreviewLayer()
                 }
                 
-                // 초기설정이 끝나면 라이브 비디오를 시작할 수 있음
+               
                 self.videoCapture.start()
             }
         }
