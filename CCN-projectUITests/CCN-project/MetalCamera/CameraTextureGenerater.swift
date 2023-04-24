a56b2b238bc3e36d49eaf6453d0dc77cd3943f72
Narasimha Naidu Kommi <nkommi@uncc.edu>
2023-04-23 22:13:33 -0400

    Refactored the Folder Structure

---
+++ CCN-projectUITests/CCN-project/MetalCamera/CameraTextureGenerater.swift
@@ -0,0 +1,45 @@
+
+
+import CoreMedia
+
+class CameraTextureGenerater: NSObject {
+    
+    public let sourceKey: String
+    var videoTextureCache: CVMetalTextureCache?
+    
+    public init(sourceKey: String = "camera") {
+        self.sourceKey = sourceKey
+        super.init()
+
+        CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, sharedMetalRenderingDevice.device, nil, &videoTextureCache)
+    }
+    
+    func texture(from cameraFrame: CVPixelBuffer) -> Texture? {
+        guard let videoTextureCache = videoTextureCache else { return nil }
+
+        let bufferWidth = CVPixelBufferGetWidth(cameraFrame)
+        let bufferHeight = CVPixelBufferGetHeight(cameraFrame)
+
+        var textureRef: CVMetalTexture? = nil
+        let _ = CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                          videoTextureCache,
+                                                          cameraFrame,
+                                                          nil,
+                                                          .bgra8Unorm,
+                                                          bufferWidth,
+                                                          bufferHeight,
+                                                          0,
+                                                          &textureRef)
+        if let concreteTexture = textureRef,
+            let cameraTexture = CVMetalTextureGetTexture(concreteTexture) {
+            return Texture(texture: cameraTexture, textureKey: self.sourceKey)
+        } else {
+            return nil
+        }
+    }
+    
+    func texture(from sampleBuffer: CMSampleBuffer) -> Texture? {
+        guard let cameraFrame = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+        return texture(from: cameraFrame)
+    }
+}
---
+++ CCN-projectUITests/CCN_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CCN_projectUITestsLaunchTests.swift
+//  CCN-projectUITests
+//
+//  Created by Venkata Sai Shalini Ganni on 3/7/23.
+//
+
+import XCTest
+
+final class CCN_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
